7351aee7d0c185bb7d0b425bb23cddf1f37509a4
Sarah Kim <kimsuyon2013@gmail.com>
2023-08-04 17:19:41 -0700

    added chessboardjsx library, displayed a board on webapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chessboard from 'chessboardjsx';
 
 function App() {
   return (
     <div className="App">
+      <p>Barbie Fischer Chess Let's GOOO</p>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chessboard width={400} /> 
       </header>
     </div>
   );
